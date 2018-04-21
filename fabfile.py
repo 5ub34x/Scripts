744d6d4cc7adfb0f507e3556f8c810eb3aa772a2
5ub34x <38159182+5ub34x@users.noreply.github.com>
2018-04-21 15:44:56 +0100

    Create fabfile.py

---
+++ fabfile.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python
+
+# Twitter: @5ub34x
+
+from fabric.api import run
+from fabric.operations import sudo
+
+# pip install fabric
+
+# Name this file fabfile.py
+# Run the following command to execute:
+# fab -i "Test.pem" -u ubuntu -H 18.130.44.42 Redirector
+
+def Redirector():
+	# Update system
+	run("sudo apt update")
+	run("sudo apt dist-upgrade -y")
+
+	# Remove text from authorized_keys
+	run("sudo sed -i -n 's/^.*0\" //p' /root/.ssh/authorized_keys")
+
+	# Add GatewayPorts yes to SSH Configuration
+	sudo('echo "GatewayPorts yes" >> /etc/ssh/sshd_config')
+
+	# Restart the SSH service
+	run("sudo service ssh restart")
